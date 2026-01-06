9d49651da3540f7c22e420535153d9f4480a7a9e
JWGroup <guandadsmail@gmail.com>
2026-01-06 20:03:57 +0800

    Initial Commit

---
+++ HapticDial/HapticDialApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticDialApp.swift
+//  HapticDial
+//
+//  Created by Guandad on 2026/1/6.
+//
+
+import SwiftUI
+
+@main
+struct HapticDialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
